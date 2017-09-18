3af97470c4095a188db407d827f2a7aa16f06add
VarianAl <100669113@student.swin.edu.au>
2017-09-18 10:55:16 +0700

    add: appliance, home agent

---
+++ MainApp/src/Agents/ApplianceAgent.java
@@ -0,0 +1,195 @@
+package Agents;
+
+import java.util.Iterator;
+
+import jade.core.AID;
+import jade.core.behaviours.Behaviour;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.lang.acl.ACLMessage;
+import Agents.Other.BaseAgent;
+
+public class ApplianceAgent extends BaseAgent
+{
+	//should be vector, should store enumeration instead of int
+	int[] weather = new int[24];
+	boolean on;
+	int current, forecast;
+	
+	//initialize variables
+	private void init()
+	{
+		on = true;
+		current = 10;
+		updateWeather();
+	}
+	
+	@Override
+	protected void setup()
+	{
+		super.setup();
+		init();
+	}
+	
+	private void updateWeather()
+	{
+		//send request for weather forecast
+		ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+		msg.setContent("Weather Forecast");
+		msg.addReceiver(new AID("WeatherAgent",AID.ISLOCALNAME));
+		send(msg);
+		//wait for result
+		addBehaviour(new weatherForecastReceiver());
+	}
+	
+	private class weatherForecastReceiver extends Behaviour
+	{
+		boolean finish=false;
+		
+		@Override
+		public void action()
+		{
+			ACLMessage msg = blockingReceive();
+			if(msg!=null)
+			{
+				//example message : weather,0
+				//0=sunny, 1=cloudy
+				weather[_current_time] = Integer.parseInt(msg.getContent().substring(msg.getContent().lastIndexOf(",")+1));
+				//exit the behaviour
+				finish = true;
+			}
+		}
+
+		@Override
+		public boolean done()
+		{
+			return finish;
+		}
+	}
+	
+	private void sendCurrentUsage()
+	{
+		updateCurrentUsage();
+		//send
+		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+		msg.setContent("electricity current," + current);
+		msg.addReceiver(new AID("HomeAgent", AID.ISLOCALNAME));
+		send(msg);
+	}
+	
+	//calculate current usage and update variable
+	private void updateCurrentUsage(){}
+	
+	private void sendForecastUsage()
+	{
+		updateForecastUsage();
+		ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
+		msg.setContent("electricity forecast," + forecast);
+		msg.addReceiver(new AID("HomeAgent", AID.ISLOCALNAME));
+		send(msg);
+	}
+	
+	//calculate forecast usage and update variable
+	private void updateForecastUsage(){}
+	
+	private void sendElectricityRequest()
+	{
+		//send request for weather forecast
+		ACLMessage msg = new ACLMessage(ACLMessage.REQUEST);
+		msg.setContent("Weather Forecast");
+		msg.addReceiver(new AID("WeatherAgent",AID.ISLOCALNAME));
+		send(msg);
+		//wait for result
+		addBehaviour(new electricityReceiver());
+	}
+	
+	private class electricityReceiver extends Behaviour
+	{
+		boolean finish=false;
+		
+		@Override
+		public void action()
+		{
+			ACLMessage msg = blockingReceive();
+			if(msg!=null)
+			{
+				//example message : electricity request,1
+				//0=declined, 1=approved
+				int value = Integer.parseInt(msg.getContent().substring(msg.getContent().lastIndexOf(",")+1));
+				if(value==0){System.out.println("Request declined by HomeAgent");}
+				else if(value==1)
+				{
+					on = true;
+					//start current usage counting, error
+					//myAgent.addBehaviour(new parallelTickerBehaviour());
+				}
+				//exit the behaviour
+				finish = true;
+			}
+		}
+
+		@Override
+		public boolean done()
+		{
+			return finish;
+		}
+	}
+	
+	private void turn(boolean on)
+	{
+		if(this.on!=on)
+		{
+			if(on==true)
+			{
+				//send electricity request to home agent
+				//home agent check current usage with max usage
+				//if current + request < max usage, approve
+				sendElectricityRequest();
+			}
+			else if(on==false)
+			{
+				on = false;
+				//stop current usage increment
+			}
+		}
+	}
+	
+	//use parallel behaviour to count time
+	//update current usage every minute
+	private class parallelTickerBehaviour{}
+	
+	//TODO UnhandledMessage function
+	@Override
+	protected void UnhandledMessage(ACLMessage msg)
+	{
+		String senderName = msg.getSender().getLocalName();
+		
+		if(msg.getPerformative()==ACLMessage.REQUEST)
+		{
+			//UserAgent or HomeAgent turn on / off
+			if(msg.getContent().contains("turn"))
+			{
+				//example message : turn,0
+				//0=off, 1=on
+				int intValue = Integer.parseInt(msg.getContent().substring(msg.getContent().lastIndexOf(",")+1));
+				boolean booleanValue = false;
+				if(intValue==0){booleanValue = false;}
+				else if(intValue==1){booleanValue = true;}
+				turn(booleanValue);
+			}
+			else{sendNotUndersood(msg,"Sorry?");}
+		}
+		else{sendNotUndersood(msg,"Sorry?");}
+	}
+	
+	//TODO TimeExpiringIn function
+	@Override
+	protected void TimeExpiringIn(int expireTimeMS){}
+
+	//TODO TimeExpired function
+	@Override
+	protected void TimeExpired(){}
+
+	//TODO SaleMade function
+	@Override
+	protected void SaleMade(ACLMessage msg){}
+}
---
+++ MainApp/src/Agents/HomeAgent.java
@@ -0,0 +1,128 @@
+package Agents;
+
+import java.util.Vector;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.lang.acl.ACLMessage;
+import Agents.Other.BaseAgent;
+//import java.lang.*;
+
+public class HomeAgent extends BaseAgent
+{
+	//number of appliances
+	int n;
+	//TODO use vector
+	//electricity usage for each time slice for each appliances, should be vector for scalability
+	int electricityUsage[][] = new int[24][n];
+	//electricity forecast for next time slice for each appliance, should be vector for scalability
+	int electricityForecast[][] = new int[24][n];
+	//list of appliance agent name, should be vector
+	String[] applianceName = new String[9];
+	
+	//init all variable value
+	//TODO init function
+	private void init()
+	{
+		//should get from argument
+		n = 1;
+		//should get from argument
+		applianceName[0] = "lamp1";
+	}
+	
+	private int getApplianceID(String name)
+	{
+		int i;
+		for(i=0;i<n;i++)
+			{if(name.equals(applianceName[i]))
+				{return i;}}
+		return -1;
+	}
+	
+	@Override
+	protected void setup()
+	{
+		super.setup();
+		init();
+		addBehaviour(new welcomeMessage());
+		//addBehaviour(new msgReceivingBehaviour());
+		System.out.println(getLocalName() + " setup is completed!");
+	}
+	
+	private class welcomeMessage extends OneShotBehaviour
+	{
+		@Override
+		public void action()
+		{
+			System.out.println(getLocalName() + " is now up and running!");
+		}
+	}
+	
+	private void sleep(int duration)
+	{
+		try{Thread.sleep(duration);}
+		catch(InterruptedException e){e.printStackTrace();}
+	}
+	
+	//forecast electricity needs for next day(x=1), next week(x=2), or next month(x=3)
+	//TODO forecast function
+	private int forecast(int x)
+	{
+		int result=0;
+		//next day forecast
+		if(x==1){}
+		//next week forecast
+		else if(x==2){}
+		//next month forecast
+		else if(x==3){}
+		return result;
+	}
+	
+	//when use too much electricity, Home agent could turn off unimportant appliances to prevent overload
+	//TODO energySaverMode function
+	private void energySaverMode(){}
+
+	@Override
+	protected void UnhandledMessage(ACLMessage msg)
+	{
+		String senderName = msg.getSender().getLocalName();
+		
+		if(msg.getPerformative()==ACLMessage.INFORM)
+		{
+			if(msg.getContent().contains("electricity"))
+			{
+				int applianceID = getApplianceID(senderName);
+				int value = Integer.parseInt(msg.getContent().substring(msg.getContent().lastIndexOf(",")+1));
+				if(msg.getContent().contains("current"))
+				{
+					//example message : electricity current,10
+					//store in electricityUsage
+					electricityUsage[_current_time][applianceID] = value;
+				}
+				else if(msg.getContent().contains("forecast"))
+				{
+					//example message : electricity forecast,12
+					//store in electricity
+					electricityForecast[_current_time][applianceID] = value;
+				}
+			}
+			else
+				{sendNotUndersood(msg,"Sorry?");}
+		}
+	}
+
+	//TODO TimeExpiringIn function
+	@Override
+	protected void TimeExpiringIn(int expireTimeMS){}
+
+	//TODO TimeExpired function
+	@Override
+	protected void TimeExpired(){}
+
+	//TODO SaleMade function
+	@Override
+	protected void SaleMade(ACLMessage msg){}
+}
+
+//TODO behaviour to negotiate price for buy & sell
