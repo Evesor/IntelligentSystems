ae7fb07f1feda2c118e4c156c3caff9c0b9a9f88
Evesor <charles.harold@homtail.com>
2017-09-18 18:35:15 +1000

    Make branch
    thing0

---
+++ MainApp/src/Helpers/Weather.java
@@ -0,0 +1,14 @@
+package Helpers;
+
+import java.io.Serializable;
+
+/******************************************************************************
+ *  Enums for types of weather we can have in our system
+ *****************************************************************************/
+public enum Weather implements Serializable {
+    VerySunny ,Sunny, Overcast, Night;
+
+    public static Weather getRandom() {
+        return values()[(int) (Math.random() * values().length)];
+    }
+}
