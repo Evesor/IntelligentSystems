8247da1d3da0010be27d26d35dc57700650e9947
dotnal <p.hct23@gmail.com>
2017-09-23 19:58:40 +1000

    add: makeshift test harness

---
+++ backend/control-server/src/main/kotlin/edu/swin/hets/tools/JadeTestHarness.kt
@@ -0,0 +1,15 @@
+package edu.swin.hets.tools
+
+import edu.swin.hets.Application
+
+/**
+ * For lack of a proper testing environment
+ */
+class JADETestHarness {
+
+}
+
+fun main(args: Array<String>) {
+    val app = Application(args)
+    app.start()
+}
