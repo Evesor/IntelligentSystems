ccf2e4732332cd87c09f2095af87189d591d0d9c
dotnal <p.hct23@gmail.com>
2017-09-30 15:13:41 +1000

    fix: proper json data objects for system definition

--- backend/control-server/src/main/kotlin/edu/swin/hets/controller/distributor/ContainerDistributor.kt
+++ backend/control-server/src/main/kotlin/edu/swin/hets/controller/distributor/ContainerDistributor.kt
@@ -1,29 +1,10 @@
 package edu.swin.hets.controller.distributor
 
-import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.core.JsonProcessingException
-import com.fasterxml.jackson.databind.DeserializationContext
-import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer
-import com.fasterxml.jackson.databind.node.IntNode
-import java.io.IOException
+import java.io.Serializable
 
-data class AgentDefinition(val name: String, val className: Class<*> , val arguments: String = ""){
-    constructor(name: String, className: String, arguments: String) : this(name, Class.forName(className), arguments)
-}
-data class ContainerDefinition(val name: String, val agents: List<AgentDefinition>)
-
-class ContainerDeserializer @JvmOverloads constructor(valueClass: Class<*>? = null) : StdDeserializer<ContainerDefinition>(valueClass) {
-    @Throws(IOException::class, JsonProcessingException::class)
-    override fun deserialize(jp: JsonParser, ctxt: DeserializationContext): ContainerDefinition {
-        val node = jp.codec.readTree<JsonNode>(jp)
-        val id = (node.get("id") as IntNode).numberValue() as Int
-        val itemName = node.get("itemName").asText()
-        val userId = (node.get("createdBy") as IntNode).numberValue() as Int
-
-        return ContainerDefinition("Placeholder", listOf())
-    }
-}
+data class AgentDefinition(val name: String, val className: String, val arguments: String = "") : Serializable
+data class ContainerDefinition(val name: String, val agents: List<AgentDefinition>) : Serializable
+data class SystemDefinition(val containers: List<ContainerDefinition>) : Serializable
 
 interface ContainerDistributor {
 
