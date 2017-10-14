735c2d310a7869c9b448c5cfb6781f82dae61934
dotnal <p.hct23@gmail.com>
2017-10-14 14:40:35 +1100

    add: intermediate work on agent runner function

--- backend/control-server/src/main/kotlin/edu/swin/hets/controller/distributor/LocalContainerDistributor.kt
+++ backend/control-server/src/main/kotlin/edu/swin/hets/controller/distributor/LocalContainerDistributor.kt
@@ -1,10 +1,14 @@
 package edu.swin.hets.controller.distributor
 
+import edu.swin.hets.agent.ApplianceAgent
+import edu.swin.hets.agent.HomeAgent
 import jade.core.Profile
 import jade.core.ProfileImpl
 import jade.core.Runtime
+import jade.wrapper.ContainerController
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.*
 
 class LocalContainerDistributor(
         runtime: Runtime,
@@ -30,5 +34,34 @@ class LocalContainerDistributor(
                 ).start()
             }
         }
+    }
+
+    fun startUpAgents(containerController: ContainerController, agentDefinition: List<AgentDefinition>) {
+        val applianceAgents = agentDefinition.filter {
+            ApplianceAgent::class.java.isAssignableFrom(Class.forName(it.className))
+        }
+
+        val homeAgents = agentDefinition.filter {
+            HomeAgent::class.java.isAssignableFrom(Class.forName(it.className))
+        }
+
+        // Ensure all agents have an owner defined
+        if (applianceAgents.any { it.owner.isBlank() }) {
+            throw IllegalStateException("All appliance agents should have an owner!")
+        }
+
+        // Ensure that all agents have a valid owner
+        if (applianceAgents.any { (name) ->
+            homeAgents.none { name == it.name }
+        }) {
+            throw IllegalStateException("Appliance owner can't be found!")
+        }
+
+        while (!(applianceAgents as Stack).empty()) {
+            val appliance = applianceAgents.pop()
+
+//            val owner = agentDefinition.first { appliance.owner == it.name }
+        }
+
     }
 }
