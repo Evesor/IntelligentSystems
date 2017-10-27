fa54406650c2b18d72c72af157bfc1aecd58fb18
dotnal <p.hct23@gmail.com>
2017-10-27 16:50:07 +1100

    fix: concurrency issues w/ websocket

--- backend/control-server/src/main/kotlin/edu/swin/hets/web/ClientWebSocketHandler.kt
+++ backend/control-server/src/main/kotlin/edu/swin/hets/web/ClientWebSocketHandler.kt
@@ -5,15 +5,16 @@ import org.eclipse.jetty.websocket.api.WebSocketException
 import org.eclipse.jetty.websocket.api.annotations.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.*
 
 @WebSocket
-class ClientWebSocketHandler: WebSocketHandler {
+class ClientWebSocketHandler : WebSocketHandler {
     companion object {
         val logger: Logger = LoggerFactory.getLogger(ClientWebSocketHandler::class.java)
         const val PATH: String = "/ws"
     }
 
-    private val sessionList: MutableList<Session> = mutableListOf()
+    private val sessionList: MutableList<Session> = Collections.synchronizedList(mutableListOf())
 
     @Throws(Exception::class)
     @OnWebSocketConnect
@@ -44,20 +45,22 @@ class ClientWebSocketHandler: WebSocketHandler {
      */
     override fun broadcast(message: String) {
         val invalidSessions: MutableList<Session> = mutableListOf()
-        sessionList.forEach {
-            try {
-                it.remote.sendString(message)
-            } catch (wse: WebSocketException) {
-                logger.info(wse.toString())
-                logger.info("scheduling to remove connection: ${it.remoteAddress}")
-                invalidSessions.add(it)
+        synchronized(sessionList) {
+            sessionList.forEach {
+                try {
+                    it.remote.sendString(message)
+                } catch (wse: WebSocketException) {
+                    logger.info(wse.toString())
+                    logger.info("scheduling to remove connection: ${it.remoteAddress}")
+                    invalidSessions.add(it)
+                }
             }
-        }
 
-        //Clean up invalid sessions (is this even necessary?)
-        invalidSessions.forEach {
-            it.disconnect()
-            sessionList.remove(it)
+            //Clean up invalid sessions (is this even necessary?)
+            invalidSessions.forEach {
+                it.disconnect()
+                sessionList.remove(it)
+            }
         }
     }
 }
